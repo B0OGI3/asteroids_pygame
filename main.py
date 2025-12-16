f117fedb047f19b950ac7036fe4368325b276354
B0OGI3 <iandavis1504@gmail.com>
2025-12-16 16:18:48 -0500

    Added working clock for frame timing

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from logger import log_state
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Window
+    clock = pygame.time.Clock() # In game clock
+    dt = 0 # Delta Time (i.e. time passed since last frame drawn)
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -16,14 +18,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
+
         log_state()
         screen.fill("black")
         pygame.display.flip()
-
-
-        
-
+        rt = clock.tick(60) # pauses the game loop until 1/60th of a second has passed 
+        dt = rt / 1000
 
 if __name__ == "__main__":
     main()
