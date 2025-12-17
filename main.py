367693e0d13b30844c13c96c05c7557b5ae26bb2
B0OGI3 <iandavis1504@gmail.com>
2025-12-16 20:10:00 -0500

    Added player and circleshape classes, can render still player object on screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state 
+from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -8,19 +9,21 @@ def main():
     clock = pygame.time.Clock() # In game clock
     dt = 0 # Delta Time (i.e. time passed since last frame drawn)
 
+    ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     # Main Game Loop
     while True:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         log_state()
         screen.fill("black")
+        ship.draw(screen)
         pygame.display.flip()
         rt = clock.tick(60) # pauses the game loop until 1/60th of a second has passed 
         dt = rt / 1000
